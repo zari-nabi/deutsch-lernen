f16f3bec5d51c699da61f684d31fb574d4160c22
unknown <Nabi@nabi-it.esfahanmet.net>
2019-02-26 13:03:30 +0330

    install reduc - axios

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, withRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
+import store from './store';
 import App from './App';
 
 const AppWithRouter = withRouter(App);
 
 ReactDOM.render(
-    <BrowserRouter>
-        <AppWithRouter />
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <AppWithRouter />
+        </BrowserRouter>
+    </Provider>
+
 
     , document.getElementById('root'));
 
---
+++ src/store/index.js
@@ -0,0 +1,10 @@
+import {createStore} from 'redux';
+
+const reducer = (state = {}, action) => {
+    return state;
+};
+
+const store = createStore(reducer);
+
+export default store;
+
